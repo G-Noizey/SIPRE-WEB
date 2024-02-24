22e47c316ec76dca848daf930d92758484f27917
Noizey <moymorcol98@gmail.com>
2024-02-23 22:56:39 -0600

    login almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-// App.jsx
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './components/Admin/Login';
+import Login from './components/Auth/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+    <Login />
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -16,28 +16,66 @@ const Login = () => {
     // Enviar la información de login al backend
     // ...
 
-    if (login exitoso) {
-      setSuccess('Inicio de sesión exitoso');
-      // Redirigir a la página principal
-    } else {
-      setError('Usuario o contraseña incorrectos');
-    }
+   
   };
 
   // Renderizado del componente
+
   return (
-    <div>
-      <h1>Iniciar sesión</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Usuario:</label>
-        <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <label htmlFor="password">Contraseña:</label>
-        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Iniciar sesión</button>
-      </form>
-      {error && <p className="error">{error}</p>}
-      {success && <p className="success">{success}</p>}
+  <section className="vh-100" style={{ backgroundColor: 'white' }}>
+
+  <div className="container py-5 h-100 border" style={{}}   >
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col col-xl-10">
+        <div className="card">
+          <div className="row g-0">
+
+          <div className="col-md-6 col-lg-5 d-none d-md-block" style={{backgroundColor: '#2D7541'}} >
+                  <img src="logo2.png" alt="login form" className="img-fluid" style={{ borderRadius: '1rem 0 0 1rem' }} />
+                </div>
+            <div className="col-md-6 col-lg-7 d-flex align-items-center">
+              <div className="card-body p-4 p-lg-5 text-black text-center"> {/* Agregar text-center para centrar el contenido */}
+                <form>
+
+                  <div className="d-flex align-items-center mb-3 pb-1">
+                    <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+
+                    <img src="logo2.png" alt="Descripción de la imagen" />
+
+                  </div>
+
+                  <h1 className="fw-bold mb-3 pb-3" style={{ letterSpacing: '1px' }}>Inicia Sesión</h1>
+                  <div className="form-outline mb-4">
+                    <input type="email" id="form2Example17" className="form-control form-control-lg" />
+                    <label className="form-label" htmlFor="form2Example17">Usuario</label>
+                  </div>
+
+                  <div className="form-outline mb-4">
+                    <input type="password" id="form2Example27" className="form-control form-control-lg" />
+                    <label className="form-label" htmlFor="form2Example27">Contraseña</label>
+                  </div>
+
+                  <div className="form-outline mb-4">
+                  <a className="small text-muted" href="#!">¿Olvidaste tu contraseña?</a>
+
+                  </div>
+
+
+                  <div className="pt-1 mb-4">
+                    <button className="btn btn-lg shadow-lg" type="button" style={{backgroundColor: '#2D7541', color: 'white', padding: '5px 150px', margin: '10px'   }}   >Iniciar</button>
+                  </div>
+                
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</section>
+
+  
   );
 };
 
